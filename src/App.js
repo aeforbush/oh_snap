f8f50f67de5075ecfd8e805c731bbcd41e621af5
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-11-30 12:39:26 -0700

    added PhotoList component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+// import PhotoList from './components/PhotoList';
 // import logo from './logo.svg';
 import './App.css';
 
@@ -24,7 +25,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
   
